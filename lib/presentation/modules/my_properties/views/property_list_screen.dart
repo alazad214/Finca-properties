c4cd0d870f39ea13741bbc46fc4bc93cb2644de3
al azad <alazad214@gmail.com>
2024-09-01 14:30:36 +0600

    my properties added

--- lib/presentation/modules/my_properties/views/property_list_screen.dart
+++ lib/presentation/modules/my_properties/views/property_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:universe_it_project/presentation/modules/my_properties/views/accepted_property.dart';
 import 'package:universe_it_project/presentation/modules/my_properties/views/pending_properties.dart';
 import 'package:universe_it_project/presentation/modules/my_properties/views/rejected_properties.dart';
+import 'package:universe_it_project/utils/app_color.dart';
 
 class PropertyListScreen extends StatelessWidget {
   PropertyListScreen({super.key});
@@ -11,27 +12,51 @@ class PropertyListScreen extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "My Properties",
-              style: TextStyle(fontSize: 16.0),
-            ),
-            titleSpacing: 0,
-            bottom: TabBar(
-              labelStyle:
-                  TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-              tabs: [
-                Tab(text: 'Accepted'),
-                Tab(text: 'Pending'),
-                Tab(text: 'Rejected'),
-              ],
-            ),
+        appBar: AppBar(
+          title: const Text(
+            'My Properties',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
           ),
-          body: TabBarView(children: [
+          centerTitle: true,
+          backgroundColor: AppColor.baseColor,
+          elevation: 4,
+          bottom: TabBar(
+            isScrollable: true,
+            indicatorColor: Colors.white,
+            indicatorWeight: 4.0,
+            labelStyle: const TextStyle(
+              fontSize: 16.0,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            unselectedLabelColor: Colors.white24,
+            tabs: const [
+              Tab(
+                text: 'Accepted',
+                icon: Icon(Icons.check_circle_outline),
+              ),
+              Tab(
+                text: 'Pending',
+                icon: Icon(Icons.hourglass_empty),
+              ),
+              Tab(
+                text: 'Rejected',
+                icon: Icon(Icons.cancel_outlined),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
             AcceptedProperty(),
             PendingProperties(),
-            RejectedProperties()
-          ])),
+            RejectedProperties(),
+          ],
+        ),
+      ),
     );
   }
 }
